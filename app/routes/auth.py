7414b6dece0d41983f57d9c138584b848fa5d665
henricobzt <henricope792@gmail.com>
2025-04-11 21:35:29 -0300

    Adicionando refresh de token e refactor na função de login_for_access_token

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,7 +1,7 @@
 from http import HTTPStatus
 
 from app.models import UserModel
-from app.security import  verify_password, generation_access_token
+from app.security import  verify_password, generation_access_token,get_current_user
 from app.database import get_db
 from app.schemas.user_schema import UserCreate, Token
 
@@ -10,28 +10,36 @@ from typing import Annotated
 from fastapi import APIRouter, HTTPException, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy import select
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 
 
 router = APIRouter(prefix='/auth', tags=['auth'])
 
-T_Session = Annotated[Session,Depends(get_db)] 
+T_asyncsession = Annotated[AsyncSession,Depends(get_db)] 
 T_formdata = Annotated[OAuth2PasswordRequestForm,Depends()]
 
-@router.post('/', response_model=Token)
-def login_for_access_token(form_data:T_formdata ,session: T_Session):
-    stmt = select(UserModel).where(UserModel.username == form_data.username)
+@router.post('/token', response_model=Token)
+async def login_for_access_token(form_data:T_formdata ,session: T_asyncsession):
+    stmt = select(UserModel).where(UserModel.email == form_data.username)
 
-    user_db = session.scalar(stmt)
+    user_db = await session.scalar(stmt)
 
     if not user_db or not verify_password(form_data.password, user_db.hashed_password):
         raise HTTPException(
-            status_code = HTTPStatus.BAD_REQUEST,
+            status_code = HTTPStatus.UNAUTHORIZED,
             detail = 'password or email incorrect'
         )
     
     access_token = generation_access_token({'sub': user_db.email})
 
-    return {'access_token': access_token, 'token_type': 'Bearer'}
+    return {'access_token': access_token, 'token_type': 'Bearer'} 
+
+@router.post('/refresh_token', response_model=Token)
+async def refresh_token(
+    user: UserModel = Depends(get_current_user)
+    ):
+    new_access_token = generation_access_token({'sub': user.email}) 
+
+    return {'access_token':new_access_token, 'token_type': 'Bearer'}
     
