c2e413b8e5e0a1adc4aacdb397d0ae1140861285
henricobzt <henricope792@gmail.com>
2025-04-11 21:32:56 -0300

    Implementação da rota de Despesas(Expenses)

--- app/main.py
+++ app/main.py
@@ -1,15 +1,16 @@
 from fastapi import FastAPI
 from http import HTTPStatus
-from app.routes import users, auth
+from app.routes import users, auth,expenses
 
 
 app = FastAPI()
 
 app.include_router(users.router, prefix='/api')
 app.include_router(auth.router, prefix='/api')
+app.include_router(expenses.router, prefix='/api')
 
 
 @app.get('/')
 def read_root():
-    return {'message': 'Olá Mundo!'}
+    return {'message': 'API ONLINE'}
 
