939c936a879f35d14388b5147c5b1074d2ce0dc7
henricobzt <henricope792@gmail.com>
2025-05-01 08:31:34 -0300

    Adicionando schemas e routes para MonthlyIncome(Definir Renda mensal))

---
+++ app/schemas/monthlyincome_schema.py
@@ -0,0 +1,18 @@
+from pydantic import ConfigDict, BaseModel, Field
+from app.schemas.basemodel_config import MyBaseModel
+from datetime import date
+
+class MonthlyIncomeCreate(MyBaseModel):
+    net_balance: int = Field(gt=0,description='Renda mensal') 
+    initial_date: date = Field(default_factory=date.today)
+
+class MonthlyIncomePublic(MonthlyIncomeCreate):
+    pass
+
+class MonthlyIncomeList(MyBaseModel):
+    monthly_incomes: list[MonthlyIncomePublic]
+    
+class MonthlyIncomeUpdate(MonthlyIncomeCreate):
+    pass
+
+
