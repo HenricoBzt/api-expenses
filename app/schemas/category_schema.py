bf5a7bea5428a5bb5cc6f9731653737555af894b
henricobzt <henricope792@gmail.com>
2025-05-01 08:32:45 -0300

    refactor: Refatoração dos schemas

--- app/schemas/category_schema.py
+++ app/schemas/category_schema.py
@@ -1,9 +1,5 @@
 from pydantic import BaseModel, ConfigDict
-
-class MyBaseModel(BaseModel):
-    model_config = ConfigDict(
-        from_attributes= True
-    )
+from app.schemas.basemodel_config import MyBaseModel
 
 class CategoryCreate(MyBaseModel):
     name: str
--- app/schemas/expenses_schemas.py
+++ app/schemas/expenses_schemas.py
@@ -1,12 +1,9 @@
 from pydantic import BaseModel, ConfigDict
 from typing import Literal, Optional
 import datetime
-from app.models import StatusType 
 
-class MyBaseModel(BaseModel):
-    model_config = ConfigDict(
-        from_attributes=True
-    )
+from app.models import StatusType 
+from app.schemas.basemodel_config import MyBaseModel
 
 class ExpenseCreate(MyBaseModel):
     category_id: int
@@ -22,12 +19,7 @@ class ExpensePublic(ExpenseCreate):
 class ExpenseList(MyBaseModel):
     expenses: list[ExpensePublic]
 
-class ExpenseUpdate(MyBaseModel):
-    category_id: int 
-    title: str
-    description: str | None = None
-    amount: float | None = None
-    date: datetime.date | None = None
-    status: StatusType | None = None
+class ExpenseUpdate(ExpenseCreate):
+    pass
 
     
