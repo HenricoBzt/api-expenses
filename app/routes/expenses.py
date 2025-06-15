9a2c05fa9321d8fd821f39bc89ac08c50de49ba1
henricobzt <henricope792@gmail.com>
2025-06-15 11:03:09 -0300

    refactor: separando lógica das rotas e implementando services

--- app/routes/expenses.py
+++ app/routes/expenses.py
@@ -2,55 +2,72 @@ from fastapi import APIRouter, Depends, HTTPException
 from http import HTTPStatus
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
-from typing import Annotated, Optional
+from typing import Annotated
+
 from app.database import get_db
 from app.security import get_current_user
-from app.models import ExpensesModel, CategoryModel, UserModel
-from app.schemas.expenses_schemas import ExpenseCreate, ExpensePublic
-from app.services.ai_classifier import classify_expense  # nosso serviço de IA
+from app.models import UserModel
+from app.schemas.expenses_schemas import ExpenseCreate, ExpensePublic,ExpenseList
+from app.crud.expenses_crud import list_expenses
+from app.services.expenses_sevices import (
+        create_expense_and_update_income,
+        update_expense_and_update_income,
+        delete_expense_and_update_income
+        )
 
-router = APIRouter(prefix="/expenses", tags=["expenses"])
+router = APIRouter(prefix='/expenses', tags=["expenses"])
 
 T_asyncsession   = Annotated[AsyncSession, Depends(get_db)]
 T_current_user   = Annotated[UserModel,    Depends(get_current_user)]
 
 
-@router.post("/", response_model=ExpensePublic, status_code=HTTPStatus.CREATED)
-async def create_expense(
+@router.post('/', response_model=ExpensePublic, status_code=HTTPStatus.CREATED)
+async def exp_create(
     session: T_asyncsession,
     current_user: T_current_user,
     expense: ExpenseCreate
-):
-
-    if not expense.category_id:
-        suggested = await classify_expense(expense.description)
-
-        stmt = select(CategoryModel.id).where(
-            CategoryModel.user_id == current_user.id,
-            CategoryModel.name.ilike(suggested)
-        )
-        result = await session.scalar(stmt)
-        if result:
-            expense.category_id = result
-        else:
-            stmt2 = select(CategoryModel.id).where(
-                CategoryModel.user_id == current_user.id,
-                CategoryModel.name == "Outros"
-            )
-            expense.category_id = await session.scalar(stmt2)
+    ):
+        return await create_expense_and_update_income(session,current_user,expense)
 
 
-    db_expense = ExpensesModel(
-        user_id     = current_user.id,
-        category_id = expense.category_id,
-        title       = expense.title,
-        description = expense.description,
-        amount      = expense.amount,
-        date        = expense.date,
-        status      = expense.status,
-    )
-    session.add(db_expense)
-    await session.commit()
-    await session.refresh(db_expense)
+@router.get('/',response_model=ExpenseList)
+async def exp_list(
+        session: T_asyncsession,
+        current_user:T_current_user,
+        skip: int = 0,
+        limit: int = 100
+        ):
 
-    return db_expense
+            return await list_expenses(session,current_user,skip,limit)
+
+
+@router.patch('/{expense_id}',response_model=ExpensePublic)
+async def exp_update(
+        expense_id:int,
+        session:T_asyncsession,
+        current_user:T_current_user,
+        expense_data:ExpenseCreate
+        ):
+            return await update_expense_and_update_income(
+                    session,
+                    current_user,
+                    expense_id,
+                    expense_data
+                    )
+
+
+@router.delete('/{expense_id}')
+async def exp_delete(
+        expense_id:int,
+        session:T_asyncsession,
+        current_user:T_current_user,
+        ):
+            return await delete_expense_and_update_income(
+                    session,
+                    current_user,
+                    expense_id
+                    )
+
+
+
+
