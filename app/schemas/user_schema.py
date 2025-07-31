65120a0ef33e9dda6dacc491c9131e5679f88af4
henricobzt <henricope792@gmail.com>
2025-07-30 21:05:35 -0300

    fix: Id validator

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel, EmailStr, ConfigDict
 
 class UserPublic(BaseModel):
+    id: int
     username: str
     email: EmailStr
     model_config = ConfigDict(from_attributes=True)
